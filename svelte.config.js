c730e82f8f8349a04a265bce4e2f1a26c52b3e54
Achaz91 <ngwouanza@gmail.com>
2025-08-11 19:55:27 +0100

    Version initiale du projet final -> adaoter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
     kit: {
-        adapter: adapter({
-            // CORRECTION : On ajoute un fallback pour les SPA
-            fallback: 'index.html',
-            pages: 'build',
-            assets: 'build',
-            precompress: false,
-            strict: true
-        })
+        // On utilise l'adaptateur automatique pour une compatibilité parfaite avec Vercel
+        adapter: adapter()
     }
 };
 
